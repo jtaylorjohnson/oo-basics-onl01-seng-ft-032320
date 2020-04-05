56b009c2b6a5233fa017f6039c9f8b0831ceb965
James Johnson <jtaylorjohnson@gmail.com>
2020-04-05 00:24:00 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,12 +3,12 @@ class Book
   attr_accessor :author 
   attr_accessor :page_count 
   attr_accessor :genre 
+  
   def initialize(title)
     @title = title 
   end
-
-
   
+  def turn_page 
+  end 
   
-
 end
